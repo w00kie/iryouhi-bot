6801229fbde606860b2c725996d2e147e2df79fa
François Rejeté <francois@rejete.com>
2024-07-03 21:26:36 +0900

    enable sqlite WAL mode in initial migration

--- prisma/migrations/20240621122354_init/migration.sql
+++ prisma/migrations/20240621122354_init/migration.sql
@@ -23,3 +23,12 @@ CREATE TABLE "Receipt" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_telegram_id_key" ON "User"("telegram_id");
+
+-- Optimize SQLite engine
+PRAGMA journal_mode=WAL;
+PRAGMA synchronous=NORMAL;
+PRAGMA busy_timeout=5000;
+PRAGMA temp_store=MEMORY;
+PRAGMA mmap_size=134217728;
+PRAGMA journal_size_limit=67108864;
+PRAGMA cache_size=2000;
