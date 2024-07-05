341c3146aff1a2c134be7e4cf6520223035cb9eb
François Rejeté <francois@rejete.com>
2024-07-05 17:06:38 +0900

    fix unescaped character in /help command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -23,7 +23,7 @@ myCommands.command("help", (ctx) =>
       "/help \\- Show this help message\n" +
       "/stats \\- Show the number of receipts saved and total amount for current year\n" +
       "/export \\[YYYY\\] \\- Export all receipts for a given year as an Excel file \\(defaults to current year\\)\n\n" +
-      "To enter a receipt, simply send a photo.",
+      "To enter a receipt, simply send a photo\\.",
     { parse_mode: "MarkdownV2" },
   ),
 );
